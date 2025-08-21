f8910c125de175b031d76e4ba13eaee5e01ebe00
Vũ Tiến Trung <vutientrung.it@gmail.com>
2025-08-21 14:13:21 +0700

    init

---
+++ src/main/java/com/foodbe/FoodBeApplication.java
@@ -0,0 +1,13 @@
+package com.foodbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodBeApplication.class, args);
+    }
+
+}
